9d07287282f00643dcb99d425b8b90b88c9424ac
Yusuke KON <yusukedrive@gmail.com>
2014-01-17 10:54:12 +0900

    地球アニメーション追加

--- Resources/practice.js
+++ Resources/practice.js
@@ -1,27 +1,96 @@
 (function() {
-  var camera, cube, geometry, height, material, renderer, scene, width;
+  var ambient, axis, camera, controls, count, cube, cubes, geometry, height, i, light, material, pGeometry, pMaterial, plane, render, renderer, sGeometry, sMaterial, scene, sphere, width, _i;
 
-  width = 500;
+  width = 700;
 
-  height = 300;
+  height = 700;
+
+  cubes = [];
+
+  count = 10;
+
+  i;
 
   scene = new THREE.Scene();
 
+  for (i = _i = 0; _i <= 9; i = ++_i) {
+    geometry = new THREE.CubeGeometry(50, 50, 50);
+    material = new THREE.MeshLambertMaterial({
+      color: "red"
+    });
+    cubes[i] = new THREE.Mesh(geometry, material);
+    cubes[i].position.set(0 + Math.random(100) * 50, 50 + Math.random(100) * 50, 0 + Math.random(100) * 50);
+    cubes[i].castShadow = true;
+    scene.add(cubes[i]);
+  }
+
   geometry = new THREE.CubeGeometry(50, 50, 50);
 
-  material = new THREE.MeshBasicMaterial({
+  material = new THREE.MeshLambertMaterial({
     color: "red"
   });
 
   cube = new THREE.Mesh(geometry, material);
 
-  cube.position.set(0, 0, 0);
+  cube.position.set(0, 50, 0);
+
+  cube.castShadow = true;
 
   scene.add(cube);
 
+  sGeometry = new THREE.SphereGeometry(200, 20, 20);
+
+  sMaterial = new THREE.MeshBasicMaterial({
+    map: THREE.ImageUtils.loadTexture('./img/2000px-BlankMap-World6.png'),
+    overdraw: true
+  });
+
+  sphere = new THREE.Mesh(sGeometry, sMaterial);
+
+  sphere.position.set(100, 100, 0);
+
+  sphere.castShadow = true;
+
+  scene.add(sphere);
+
+  pGeometry = new THREE.PlaneGeometry(300, 300);
+
+  pMaterial = new THREE.MeshLambertMaterial({
+    color: "green",
+    side: THREE.DoubleSide
+  });
+
+  plane = new THREE.Mesh(pGeometry, pMaterial);
+
+  plane.position.set(0, 0, 0);
+
+  plane.rotation.x = (90.0 / 180.0) * Math.PI;
+
+  plane.receiveShadow = true;
+
+  scene.add(plane);
+
+  axis = new THREE.AxisHelper(1000);
+
+  axis.position.set(0, 0, 0);
+
+  scene.add(axis);
+
+  light = new THREE.DirectionalLight(0xffffff, 1);
+
+  light.castShadow = true;
+
+  light.position.set(0, 300, 30);
+
+  scene.add(light);
+
+  ambient = new THREE.AmbientLight(0x333333);
+
+  scene.add(ambient);
+
   camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
 
-  camera.position.set(200, 100, 500);
+  camera.position.set(200, 300, 500);
 
   camera.lookAt(cube.position);
 
@@ -29,11 +98,36 @@
 
   renderer.setSize(width, height);
 
-  renderer.setClearColor('0x000000', 1);
+  renderer.setClearColor(0xeeeeee, 1);
+
+  renderer.shadowMapEnabled = true;
 
   document.getElementById('stage').appendChild(renderer.domElement);
 
-  renderer.render(scene, camera);
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+  render = function() {
+    var _j;
+    requestAnimationFrame(render);
+    cube.rotation.x += 1 * Math.PI / 180;
+    cube.rotation.y += 1 * Math.PI / 180;
+    cube.rotation.z += 1 * Math.PI / 180;
+    cube.position.x = Math.sin(new Date().getTime() / 200) * 100;
+    cube.position.z = Math.cos(new Date().getTime() / 200) * 100;
+    cube.position.y = 100 + Math.sin(new Date().getTime() / 200) * 100;
+    for (i = _j = 0; _j <= 9; i = ++_j) {
+      cubes[i].rotation.x += (1 + Math.random(1)) * Math.PI / 180;
+      cubes[i].rotation.y += (1 + Math.random(2)) * Math.PI / 180;
+      cubes[i].rotation.z += (1 + Math.random(3)) * Math.PI / 180;
+      cubes[i].position.x = Math.sin(new Date().getTime() / 200 + Math.random() * 0.1) * 100;
+      cubes[i].position.z = Math.cos(new Date().getTime() / 200 + Math.random() * 0.1) * 100;
+      cubes[i].position.y = 100 + Math.sin(new Date().getTime() / 200 + Math.random() * 0.1) * 100;
+    }
+    controls.update();
+    return renderer.render(scene, camera);
+  };
+
+  render();
 
 }).call(this);
 
--- coffee/practice.coffee
+++ coffee/practice.coffee
@@ -1,30 +1,114 @@
-width = 500
-height = 300
+width  = 700
+height = 700
+cubes = []
+count = 10
+i
+
 
 # scene
 scene = new THREE.Scene();
-# geometry
+# geometry 立方体
+
+
+for i in [0..9]
+  geometry = new THREE.CubeGeometry(50, 50, 50);
+  material = new THREE.MeshLambertMaterial({color : "red"})
+  cubes[i] = new THREE.Mesh(geometry, material)
+  cubes[i].position.set(0+Math.random(100)*50, 50+Math.random(100)*50, 0+Math.random(100)*50)
+  # add shadow
+  cubes[i].castShadow = true
+  scene.add(cubes[i])
+
+
+
+
 geometry = new THREE.CubeGeometry(50, 50, 50);
-
-material = new THREE.MeshBasicMaterial({color : "red"})
-
+# 光を反射しないマテリアル 全面が光る感じになるテクスチャ
+# material = new THREE.MeshBasicMaterial({color : "red"})
+# 光を反射するマテリアル
+material = new THREE.MeshLambertMaterial({color : "red"})
 cube = new THREE.Mesh(geometry, material)
-cube.position.set(0, 0, 0)
-
+cube.position.set(0, 50, 0)
+# add shadow
+cube.castShadow = true
 scene.add(cube)
 
+# 球
+sGeometry = new THREE.SphereGeometry( 200, 20, 20 )
+#sMaterial = new THREE.MeshLambertMaterial({color : "blue"})
+sMaterial = new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( './img/2000px-BlankMap-World6.png' ), overdraw: true } )
+
+sphere = new THREE.Mesh(sGeometry, sMaterial)
+sphere.position.set(100, 100, 0)
+sphere.castShadow = true
+scene.add(sphere)
+
+# plane
+pGeometry = new THREE.PlaneGeometry(300, 300);
+pMaterial = new THREE.MeshLambertMaterial({color : "green",side: THREE.DoubleSide})
+plane = new THREE.Mesh(pGeometry, pMaterial)
+plane.position.set(0, 0, 0)
+# 回転
+plane.rotation.x = (90.0 / 180.0) * Math.PI
+# add shadow
+plane.receiveShadow = true
+scene.add(plane)
+
+
+# デバッグ用の軸をhelperで表示
+axis = new THREE.AxisHelper(1000)
+axis.position.set(0, 0, 0)
+scene.add(axis)
+
 # light
+light = new THREE.DirectionalLight(0xffffff, 1)
+# add shadow
+light.castShadow = true
+light.position.set(0, 300, 30)
+scene.add(light)
+
+# 環境光
+ambient = new THREE.AmbientLight(0x333333)
+scene.add(ambient)
 
 # camera
 camera = new THREE.PerspectiveCamera(45, width/height, 1, 1000)
-camera.position.set(200, 100, 500)
+camera.position.set(200, 300, 500)
 camera.lookAt(cube.position)
 
 # rendering
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height)
-renderer.setClearColor('0x000000', 1)
-
+renderer.setClearColor(0xeeeeee, 1)
+# shadow
+renderer.shadowMapEnabled = true
 document.getElementById('stage').appendChild(renderer.domElement)
 
-renderer.render(scene, camera)
+# control
+controls = new THREE.OrbitControls(camera, renderer.domElement)
+
+# animation
+render = ()->
+  requestAnimationFrame(render)
+  cube.rotation.x += 1 * Math.PI /180
+  cube.rotation.y += 1 * Math.PI /180
+  cube.rotation.z += 1 * Math.PI /180
+  cube.position.x = Math.sin(new Date().getTime()/200) * 100
+  cube.position.z = Math.cos(new Date().getTime()/200) * 100
+  cube.position.y = 100 + Math.sin(new Date().getTime()/200) * 100
+
+  for i in [0..9]
+  	#console.log cubes[i]
+  	cubes[i].rotation.x += (1 + Math.random(1)) * Math.PI /180
+  	cubes[i].rotation.y += (1 + Math.random(2)) * Math.PI /180
+  	cubes[i].rotation.z += (1 + Math.random(3)) * Math.PI /180
+  	cubes[i].position.x = Math.sin(new Date().getTime()/200 + Math.random()*0.1) * 100
+  	cubes[i].position.z = Math.cos(new Date().getTime()/200 + Math.random()*0.1) * 100
+  	cubes[i].position.y = 100 + Math.sin(new Date().getTime()/200+ Math.random()*0.1) * 100
+
+  controls.update()
+  renderer.render(scene, camera)
+
+# do animation
+render()
+#renderer.render(scene, camera)
